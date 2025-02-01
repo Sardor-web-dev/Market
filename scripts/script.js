899ba10a3d906d5c7f27e024dee8dd1ca19ed331
SarikViper <dzamolovsardor5@gmail.com>
2025-02-02 00:44:10 +0500

    added counter

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector('.container');
 const showFive = document.getElementById('five');   
 const showAll = document.getElementById('all');
-const countText = document.querySelector('.count p'); // Находим текст счетчика
+const countText = document.querySelector('.count p'); 
 
-let cartCount = 0; 
+let cartCount = 0;
+let cart = [];  
 
 
 function updateCartCount() {
     countText.innerHTML = `В корзине: ${cartCount} ${cartCount > 1 ? 'товара' : 'товар'}`;
 }
+
+
 function renderProducts(products) {
-    container.innerHTML =''
+    container.innerHTML = '';
     products.forEach(tovar => {
         const product = document.createElement('div');
         const firstSide = document.createElement('div');
@@ -41,22 +44,37 @@ function renderProducts(products) {
         txt.classList.add('details_text');
         dostavka.classList.add('dostavka');
         tt.classList.add('details_text');
-        addBtn.classList.add('added')
-        
-        addBtn.onclick = () => {
-            addBtn.classList.toggle('add');
-            addBtn.classList.toggle('added');
+        addBtn.classList.add('added');
 
+        const isAdded = cart.some(item => item.id === tovar.id);
+        if (isAdded) {
+            addBtn.innerText = 'Добавлено';
+            addBtn.classList.remove('add');
+            addBtn.classList.add('added');
+        } else {
+            addBtn.innerText = 'Добавить в корзину';
+            addBtn.classList.remove('added');
+            addBtn.classList.add('add');
+        }
+
+        addBtn.onclick = () => {
             if (addBtn.classList.contains('add')) {
                 addBtn.innerText = 'Добавлено';
+                addBtn.classList.remove('add');
+                addBtn.classList.add('added');
+                cart.push(tovar); 
                 cartCount++; 
             } else {
                 addBtn.innerText = 'Добавить в корзину';
+                addBtn.classList.remove('added');
+                addBtn.classList.add('add');
+                cart = cart.filter(item => item.id !== tovar.id); 
                 cartCount--; 
             }
 
             updateCartCount(); 
         };
+
 
         image.src = tovar.image;
         name.innerHTML = tovar.title;
@@ -67,7 +85,6 @@ function renderProducts(products) {
         txt.innerHTML = tovar.rating.rate;
         box.src = './images/box.svg';
         tt.innerHTML = tovar.rating.count;
-        addBtn.innerHTML = 'Добавить в корзину'; 
 
         price.append(dolar, text);
         stars.append(star, txt);
@@ -76,17 +93,18 @@ function renderProducts(products) {
         firstSide.append(image);
         secondSide.append(name, description, details, addBtn);
         product.append(firstSide, secondSide);
-        
+
         container.append(product);
     });
 }
-updateCartCount();
-renderProducts(arr);
+
+updateCartCount(); 
+renderProducts(arr); 
 
 showFive.onclick = () => {
-    renderProducts(arr.slice(0, 5));
+    renderProducts(arr.slice(0, 5)); 
+};
 
-}
 showAll.onclick = () => {
-    renderProducts(arr);
-}
+    renderProducts(arr); 
+};
