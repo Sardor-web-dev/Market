8cc2a0d6b433e0f529fe3e70b12b1f89a93319f1
SarikViper <dzamolovsardor5@gmail.com>
2025-02-05 23:26:08 +0500

    modal window logics

--- scripts/script.js
+++ scripts/script.js
@@ -9,25 +9,63 @@ const Close = document.getElementById("close")
 let CartList = document.querySelector(".list");
 
 let cartCount = 0;
-let cart = [];  
+let cart = [];
 
-CartBtn.onclick =() => {
+CartBtn.onclick = () => {
     CartWindow.classList.add('cart_active');
-    texts.style.display = "flex"
+    texts.style.display = "flex";
+    CartList.style.display = "flex";
+};
 
-}
-Close.onclick =() => {
+Close.onclick = () => {
     CartWindow.classList.remove('cart_active');
-    texts.style.display = "none"
+    texts.style.display = "none";
+    CartList.style.display = "none";
 
-}
-document.onclick =(e) => {
-    if(!CartWindow.contains(e.target) && e.target !== CartBtn) {
+};
+
+document.onclick = (e) => {
+    if (!CartWindow.contains(e.target) && e.target !== CartBtn) {
         CartWindow.classList.remove('cart_active');
-        texts.style.display = "none"
+    texts.style.display = "none";
+    CartList.style.display = "none";
+
     }
 };
-CartList = cart;
+function renderCart() {
+    CartList.innerHTML = ''; 
+    for (let cartProduct of cart) {
+        const product = document.createElement('div');
+        product.classList.add('product');
+
+        const firstSide = document.createElement('div');
+        firstSide.classList.add('first_side');
+        const image = document.createElement('img');
+        image.src = cartProduct.image;
+        firstSide.append(image);
+
+        const secondSide = document.createElement('div');
+        secondSide.classList.add('second_side');
+        const name = document.createElement('p');
+        name.innerHTML = cartProduct.title;
+        const description = document.createElement('span');
+        description.classList.add('description');
+        description.innerHTML = cartProduct.description;
+
+        const price = document.createElement('div');
+        price.classList.add('price');
+        const dolar = document.createElement('img');
+        dolar.src = './images/dollar.svg';
+        const text = document.createElement('span');
+        text.innerHTML = cartProduct.price;
+        price.append(dolar, text);
+
+        secondSide.append(name, description, price);
+        product.append(firstSide, secondSide);
+        CartList.append(product);
+    }
+}
+
 function updateCartCount() {
     countText.innerHTML = `В корзине: ${cartCount} ${cartCount > 1 ? 'товара' : 'товар'}`;
 };
@@ -92,6 +130,7 @@ function renderProducts(products) {
                     cartCount--; 
                 }
                 updateCartCount(); 
+                renderCart();
             };
     
     
@@ -127,4 +166,4 @@ showAll.onclick = () => {
     renderProducts(arr); 
 };
 
-console.log(CartList);
+
